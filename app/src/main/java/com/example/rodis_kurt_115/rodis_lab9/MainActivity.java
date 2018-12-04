c012edd5c7d182860ab2033f1fcb0015c3758bf9
unknown <RODIS_KURT_115@IICSGENERAL.com>
2018-12-04 15:30:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rodis_kurt_115/rodis_lab9/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.rodis_kurt_115.rodis_lab9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
